31cf528a105bec331a818376a25d819bdf18e28f
Dj Pearson <92533795+dj-pearson@users.noreply.github.com>
2025-08-05 14:41:06 +0000

    feat: Replace RoleBasedSidebar with a unified Sidebar component for improved navigation

--- client/src/components/layout/layout.tsx
+++ client/src/components/layout/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import RoleBasedSidebar from "./role-based-sidebar";
+import Sidebar from "./sidebar";
 import Header from "./header";
 import Footer from "./footer";
 
@@ -10,7 +10,7 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen bg-background flex">
-      <RoleBasedSidebar />
+      <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header />
         <main className="flex-1 p-6">
